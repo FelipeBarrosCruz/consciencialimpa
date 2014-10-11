d8f900d4e49103162b1e24f76fbcb85e9b0efc7e
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2014-10-11 13:55:07 -0300

    Put route provider in app

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,16 @@
 "use scrict";
 
 var app = angular.module('conscienciaApp', ['ui.bootstrap']);
-const APP_PAGE = 'html/page';
-const APP_TPL  = 'html/tpl';
+const APP_PAGE = 'html/pages/';
+const APP_TPL  = 'html/tpls/';
 
-app.config(function($routeProvider, $locationProvider) {
-	$routeProvider.
-		when('/', {controller: 'HomeCtrl', templateUrl: })
-});
+// app.config(function($routeProvider) {
+// 	$routeProvider.
+// 		when('/', {controller: 'HomeCtrl', templateUrl: APP_PAGE + 'home.html'})
+// 		.otherwise({ redirectTo: '/' });
+// });
+
+
+// app.controller('HomeCtrl', ['$scope', function ($scope) {
+	
+// }]);
